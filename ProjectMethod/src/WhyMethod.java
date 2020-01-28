2e814af9fd0ebaa6835080da001f18fedcbfd3db
Minserku <minserku>
2020-01-28 20:02:37 +0900

    JAVAOOP

---
+++ ProjectMethod/src/WhyMethod.java
@@ -0,0 +1,20 @@
+
+public class WhyMethod {
+	
+	public static void printTwoTimes(String text) {
+		System.out.println("----");
+		System.out.println(text);
+		System.out.println(text);
+	}
+	
+	public static void main(String[] args) {
+		
+		printTwoTimes("A");
+		printTwoTimes("A");
+		printTwoTimes("B");
+		
+		
+		
+	}
+
+}
